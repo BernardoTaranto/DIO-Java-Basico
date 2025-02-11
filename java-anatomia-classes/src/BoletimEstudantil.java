1199c673438ce190b9632fda9ea9eb17f7dbf692
BernardoTaranto <bernardotaranto@gmail.com>
2025-02-10 21:12:31 -0300

    criando u, boletim estudantil

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal<6)
+            System.out.println("REPRONADO");
+        else if (mediaFinal==6)
+            System.out.println("PROVA DE MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
