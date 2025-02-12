3dde3b4ac4629c41a6148753669a0ea09aa403fb
BernardoTaranto <bernardotaranto@gmail.com>
2025-02-12 11:54:39 -0300

    Nos exemplos foram demonstrados como são os tipos de comentários e como se cria uma calculadora

---
+++ JavaTerminal/src/Calculadora.java
@@ -0,0 +1,26 @@
+/**
+ * <h1> Calculadora </h1>
+ * Á calculadora realiza operacões matmáticas entre números inteiros
+ * <p>
+ * <b> note: </b> leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author  Bernardo Taranto
+ * @version 1.0
+ * @since 01/02/2025
+ */
+
+ public class Calculadora {
+
+/**
+ * Este método é usado para somar dois números inteiros
+ * @param numeroUm este é o primeiro parâmtro do método
+ * @param numeroDois este é o segundo parâmtro do método
+ * @return int o resulrtado deste método é a soma dos dois números
+ * 
+ */
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ JavaTerminal/src/Comentarios.java
@@ -0,0 +1,20 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        
+        // Olá, sou o COMENTÁTIO DE LINHA ÚNICA
+
+
+        /*sou o comentário que posso ter mais 
+         * detalhamento quando necessário
+         *Ou seja, sou o COMENTÁRIO DE MÚLTIPLAS LINHAS
+         */
+
+
+         /**
+          * Essas duas estrelinhas acima serve para identificar
+          *que você pretende elaborar um comentário
+          * a nível de documentação.
+          * Que incrível!!!!
+          */
+    }
+}
