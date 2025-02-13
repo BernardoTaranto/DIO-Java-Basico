938dfa39bfbc45f150f98c3fa9441ec097418777
BernardoTaranto <bernardotaranto@gmail.com>
2025-02-13 08:32:49 -0300

    Nao obtive êxito nos Exemplos. Falta achar o erro

--- JavaTerminal/src/AboutMe.java
+++ JavaTerminal/src/AboutMe.java
@@ -1,9 +1,13 @@
 public class AboutMe {
     public static void main(String[] args){
+
         String nome = args[0];
         String sobrenome = args[1];
-        int idade = Integer.parseInt(args[2]); //int idade = Integer.valueOf(args[2]); foi usado esse no exemplo
-        double altura = Double.parseDouble(args[3]); //double altura = Double.valueOf(args[3]); foi usado esse no exemplo
+        int idade = Integer.parseInt(args[2]);
+        double altura = Double.parseDouble(args[3]);
+
+        /*int idade = Integer.valueOf(args[2]); foi usado esse no exemplo
+        double altura = Double.valueOf(args[3]); foi usado esse no exemplo*/
 
         System.out.println("Olá, me chamo: " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
