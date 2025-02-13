fbf776659326e4a54c3265ca046b364d569d5a5e
BernardoTaranto <bernardotaranto@gmail.com>
2025-02-12 21:19:07 -0300

    Nesse exemplo vimos como executar o comando pelo terminal. Não obtive êxito nesse exemplo :(

---
+++ JavaTerminal/src/AboutMe.java
@@ -0,0 +1,16 @@
+public class AboutMe {
+    public static void main(String[] args){
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.parseInt(args[2]); //int idade = Integer.valueOf(args[2]); foi usado esse no exemplo
+        double altura = Double.parseDouble(args[3]); //double altura = Double.valueOf(args[3]); foi usado esse no exemplo
+
+        System.out.println("Olá, me chamo: " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é: " + altura + " cm");
+
+    }
+
+
+}
+
