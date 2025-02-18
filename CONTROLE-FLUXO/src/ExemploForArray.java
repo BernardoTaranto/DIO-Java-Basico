cf9f75ae6dcc5300b55a474440992b0211dc34af
BernardoTaranto <bernardotaranto@gmail.com>
2025-02-18 19:05:01 -0300

    Nesse exemplo foi demonstrado o FOR em ARRAYS

---
+++ CONTROLE-FLUXO/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main (String[] args){
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+    
+        for(int = x; x< alunos.length; x ++){
+
+            System.out.println("O aluno no índice x= " + x + "é " + alunos[x]);
+
+        }
+    }
+}
