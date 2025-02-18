70b25891c8d0e34a19611761368f45fccff0f34d
BernardoTaranto <bernardotaranto@gmail.com>
2025-02-18 19:40:40 -0300

    atualizado

--- CONTROLE-FLUXO/src/ExemploForArray.java
+++ CONTROLE-FLUXO/src/ExemploForArray.java
@@ -1,11 +1,11 @@
 public class ExemploForArray {
-    public static void main (String[] args){
-        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
     
-        for(int = x; x< alunos.length; x ++){
-
-            System.out.println("O aluno no índice x= " + x + "é " + alunos[x]);
-
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
         }
+    
     }
 }
